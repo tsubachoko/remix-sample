c16d2c6f87e4d4f51bb6550b2a4ba1f97e603a70
chizzo <57238328+tsubachoko@users.noreply.github.com>
2023-08-11 13:01:24 +0900

    タスクの入力フォーム

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,18 +1,30 @@
-import type { V2_MetaFunction } from "@remix-run/node";
+import type { V2_MetaFunction } from "@remix-run/node"
+import { useState } from "react"
 
 export const meta: V2_MetaFunction = () => {
   return [
     { title: "New Remix App" },
     { name: "description", content: "Welcome to Remix!" },
-  ];
-};
+  ]
+}
 
 export default function Index() {
+  const [text, setText] = useState('')
+
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
       <h1>ToDo App</h1>
+      <div>
+        <input
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+          type="text"
+        />
+
+        <div>{text}</div>
+      </div>
 
       <h2>ToDo List</h2>
     </div>
-  );
+  )
 }
